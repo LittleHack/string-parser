f82d24d45219d5289dbd3184a20aac42e041103c
Ivan Zhelyazkov <k3v3rn@gmail.com>
2019-07-01 22:27:13 +0100

    Define startParse function for Int and Float parsers

--- src/data_parser.cpp
+++ src/data_parser.cpp
@@ -11,5 +11,64 @@ bool DataParser::getValidity()
     return m_validity;
 }
 
+bool IntParser::startParse()
+{
+    const char* str = m_dataStr.c_str();
+    char* tmp = NULL;
+    bool ret = true;
+    m_elementData = 0;
+
+    do
+    {
+        long longData = strtol(str, &tmp, 10);
+        if (errno != 0 || tmp != str + strlen(str) || *str == ' ')
+        {
+            ret = false;
+            break;
+        }
+
+        if (longData > INT_MAX || longData < INT_MIN)
+        {
+            ret = false;
+            break;
+        }
+
+        m_elementData = longData;
+    }
+    while (0);
+
+    return ret;
+}
+
+bool ShortParser::startParse()
+{
+    const char* str = m_dataStr.c_str();
+    char* tmp = NULL;
+    bool ret = true;
+    m_elementData = 0;
+
+    do
+    {
+        long longData = strtol(str, &tmp, 10);
+        if (errno != 0 || tmp != str + strlen(str) || *str == ' ')
+        {
+            ret = false;
+            break;
+        }
+
+        if (longData > SHORT_MAX || longData < SHORT_MIN)
+        {
+            ret = false;
+            break;
+        }
+
+        m_elementData = longData;
+    }
+    while (0);
+
+    return ret;
+}
+
+ 
 
 
