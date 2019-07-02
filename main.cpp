c54559da30d283be1fe10cd23a1b90d225cfede2
Ivan Zhelyazkov <k3v3rn@gmail.com>
2019-07-02 06:02:44 +0100

    Implement parsing logic in main function using test msg and format

--- main.cpp
+++ main.cpp
@@ -15,6 +15,42 @@ int main()
         return -1;
     }
 
+    std::string fmtStr;
+    if (!getline(fmtFile, fmtStr))
+    {
+        std::cout << "****  READ FORMAT FILE FAILED  ****" << std::endl;
+        return -1;
+    }
+
+    FormatParser fmtParser;
+    if (false == fmtParser.parse(fmtStr))
+    {
+        std::cout << "****  PARSE FORMAT FILE FAILED  ****" << std::endl;
+        return -1;
+    }
+
+    std::string msgStr;
+    MsgParser msgParser;
+    int lineNum = 1;
+    while (getline(msgFile, msgStr))
+    {
+        std::cout << "*************************************" << std::endl;
+        if (true == msgParser.parse(msgStr, fmtParser.getFormat()))
+        {
+            std::cout << "* LINE[" << lineNum << "] PARSE SUCCESS" << std::endl;
+            std::cout << msgParser.getParsedStr() << std::endl;
+        }
+        else
+        {
+            std::cout << "* LINE[" << lineNum << "] PARSE FAILED" << std::endl;
+        }
+        std::cout << "*************************************" << std::endl << std::endl;
+
+        lineNum++;
+    }
+
+    std::cout << "****  PARSE FINISHED  ****" << std::endl;
+
     return 0;
 }
 
